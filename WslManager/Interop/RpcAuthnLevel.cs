8d7db76876d81f438146bcdea35a029d4ab853fa
rkttu <rkttu@rkttu.com>
2019-08-05 01:04:18 +0900

    Add CoInitializeSecurity for accessing WSL/LXSS interface

---
+++ WslManager/Interop/RpcAuthnLevel.cs
@@ -0,0 +1,13 @@
+﻿namespace WslManager.Interop
+{
+    public enum RpcAuthnLevel
+    {
+        Default = 0,
+        None = 1,
+        Connect = 2,
+        Call = 3,
+        Pkt = 4,
+        PktIntegrity = 5,
+        PktPrivacy = 6
+    }
+}
---
+++ WslManager/Interop/RpcImpLevel.cs
@@ -0,0 +1,11 @@
+﻿namespace WslManager.Interop
+{
+    public enum RpcImpLevel
+    {
+        Default = 0,
+        Anonymous = 1,
+        Identify = 2,
+        Impersonate = 3,
+        Delegate = 4
+    }
+}
